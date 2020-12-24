3a50952b4e1ea1e845e81df40bfbe30025858e0f
Radish4649 <69351166+Radish4649@users.noreply.github.com>
2020-12-24 23:46:00 +0900

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const bg = document.querySelector('#sliding-image');
+const windowWidth = window.innerWidth / 5;
+const windowHeight = window.innerHeight / 5 ;
+
+document.querySelector(".page-grid").addEventListener('mousemove', (e) => {
+  const mouseX = e.clientX / (windowWidth * 2);
+  const mouseY = e.clientY / (windowHeight * 2);
+  
+  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+});
---
+++ scripts/results.js
@@ -0,0 +1,70 @@
+// this gets the cards from the tarot choosing screen
+var cardZero = cardList[localStorage.getItem('card zero')];
+var cardOne = cardList[localStorage.getItem('card one')];
+var cardTwo = cardList[localStorage.getItem('card two')];
+
+// this is all the functions we want to run on start up
+fillText()
+
+// fills out all three of the card results
+function fillText() {
+    document.getElementById("name-zero").innerHTML = cardZero["name"]
+    document.getElementById("meaning-zero").innerHTML = cardZero["meaning_up"]
+    document.getElementById("card-zero").style.content = getCardImage(cardZero["name_short"])
+
+    document.getElementById("name-one").innerHTML = cardOne["name"]
+    document.getElementById("meaning-one").innerHTML = cardOne["meaning_up"]
+    document.getElementById("card-one").style.content = getCardImage(cardOne["name_short"])
+
+    document.getElementById("name-two").innerHTML = cardTwo["name"]
+    document.getElementById("meaning-two").innerHTML = cardTwo["meaning_up"]
+    document.getElementById("card-two").style.content = getCardImage(cardTwo["name_short"])
+}
+
+// "Learn More" Button / pop-ups
+document.getElementById("button-zero").addEventListener("click", learnMoreClick)
+document.getElementById("button-one").addEventListener("click", learnMoreClick)
+document.getElementById("button-two").addEventListener("click", learnMoreClick)
+
+function learnMoreClick(btn) {
+    var card = whichButton(btn)
+    fillPopup(card)
+    revealPopup()
+}
+
+function whichButton(btn) {
+    switch (btn.currentTarget.id) {
+        case "button-zero":
+            return cardZero
+        case "button-one":
+            return cardOne
+        case "button-two":
+            return cardTwo
+        default:
+            return cardTwo
+    }
+}
+
+// pop { name value meaning meaning-rev description } button-wikipedia
+function fillPopup(card) {
+    document.getElementById("pop-name").innerHTML = card["name"]
+    document.getElementById("pop-value").innerHTML = `<b>Value</b>: ${card["value_int"]}`
+    document.getElementById("pop-meaning").innerHTML = card["meaning_up"]
+    document.getElementById("pop-meaning-rev").innerHTML = card["meaning_rev"]
+    document.getElementById("pop-description").innerHTML = card["desc"]
+    document.getElementById("pop-image").style.content = getCardImage(card["name_short"])
+
+    document.getElementById("form-wikipedia").setAttribute("action", card["wiki"])
+}
+
+function revealPopup() {
+    document.getElementById("pop-up-grid").style.display = "grid"
+} 
+
+// "Close" button
+document.getElementById("button-close").addEventListener("click", hidePopup)
+
+
+function hidePopup() {
+    document.getElementById("pop-up-grid").style.display = "none"
+}
